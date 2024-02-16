af24d77cd19e3313898c580b970a7493b0b7abf2
Ross Barnowski <rossbar@berkeley.edu>
2024-02-16 09:51:29 -0800

    Quickstart->walkthrough and add an abridged version.

--- conf.py
+++ conf.py
@@ -30,4 +30,4 @@ exclude_patterns = [
 
 html_theme = 'sphinx_book_theme'
 html_title = "Caltech HPC User Guides"
-html_static_path = ['_static']
+#html_static_path = ['_static']
--- index.md
+++ index.md
@@ -12,4 +12,5 @@ hidden: true
 ---
 
 quickstart
+mnist-walkthrough
 ```
